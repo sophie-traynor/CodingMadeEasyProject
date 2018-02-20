e44ae37dd659f69f7e9a4da82286b918cc3cdb13
Sophie Traynor <sophietraynor@Sophies-MacBook-Pro.local>
2018-02-20 22:06:41 +0000

    add profile image and cover photo in open session, doesnt save yet

--- CodingMadeEasyProject/HomeVC.swift
+++ CodingMadeEasyProject/HomeVC.swift
@@ -11,20 +11,23 @@ import FirebaseAuth
 import FirebaseDatabase
 import FirebaseStorage
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: - Properties
     
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var coverImage: UIImageView!
     @IBOutlet weak var displayNameTextField: UITextField!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var dobTextField: UITextField!
     
+    var imageTag: Int?
     
     ///Reference to Firebase Database
     var ref: DatabaseReference?
+    
     ///Default name for the user Display Name
     var displayName: String = ""
     var firstName: String = ""
@@ -47,6 +50,31 @@ class HomeVC: UIViewController {
         //TODO: - FIX ERROR HANDLING WHEN LOGGING OUT AFTER RELOGGING IN WITH NO DATA
         loadData()
     }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss the picker if the user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //The info dictionary may contain multiple representations of the image
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        //Set photoImageView to display the selected image
+        
+        if imageTag == 1{
+            coverImage.image = selectedImage
+        }
+        else if imageTag == 2{
+            profileImage.image = selectedImage
+        }
+       
+        //Dismiss the picker
+        dismiss(animated:true, completion: nil)
+    }
+    
     
     //MARK: - Public Functions
     
@@ -78,4 +106,25 @@ class HomeVC: UIViewController {
         }
     }
     
+    //MARK: - Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        if sender.view?.tag == 1{
+            imageTag = 1
+        }
+        else if sender.view?.tag == 2{
+            imageTag = 2
+        }
+        
+        //UIImagePickerController is a view controller that lets a user pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        
+        //Only allow photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary
+        
+        //Make sure ViewController is notified when the user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
